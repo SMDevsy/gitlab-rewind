60e838379237ef80ece42c284d82bdb078ffb252
Michał Miłek <michal.mileq@gmail.com>
2023-02-05 00:19:13 +0100

    Make the button call JS code, change background color

--- src/api_calls.js
+++ src/api_calls.js
@@ -1,94 +1,42 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-exports.__esModule = true;
-require("node-fetch");
 function getLowerDateBoundary() {
-    var today = new Date();
-    var yearAgoStr = new Date(today
-        .setFullYear(today.getFullYear() - 1))
-        .toISOString()
-        .slice(0, 10);
-    return yearAgoStr;
+  let today = new Date()
+  let yearAgoStr = new Date(today
+    .setFullYear(today.getFullYear() - 1))
+    .toISOString()
+    .slice(0, 10)
+  return yearAgoStr
 }
-function fetchIssueData(url) {
-    return __awaiter(this, void 0, void 0, function () {
-        var fetchedData;
-        return __generator(this, function (_a) {
-            console.log("Querying ".concat(url, " for Issue data"));
-            fetchedData = [];
-            return [2 /*return*/, fetch(url, {
-                    method: "GET"
-                })
-                    .then(function (res) {
-                    if (res.ok) {
-                        console.log("fetchIssueData: Success");
-                    }
-                    else {
-                        console.log("fetchIssueData: Failure");
-                    }
-                })["catch"](function (error) { return console.log("Error: ".concat(error)); })];
-        });
-    });
+
+async function fetchIssueData(url) {
+  console.log(`Querying ${url} for Issue data`)
+  //let fetchedData = []
+  return fetch(url, {
+    method: "GET"
+  })
+    .then(res => {
+      if (res.ok) {
+        console.log("fetchIssueData: Success")
+      } else { console.log("fetchIssueData: Failure") }
+    }).catch(error => console.log(`Error: ${error}`))
 }
-function getUserData(username) {
-    return __awaiter(this, void 0, void 0, function () {
-        var apiUrl, searchParamsObject, searchParams, result;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    apiUrl = "https://gitlab.com/api/v4/users/".concat(username, "/events?");
-                    searchParamsObject = {
-                        per_page: "10"
-                    };
-                    searchParams = new URLSearchParams(searchParamsObject).toString();
-                    return [4 /*yield*/, Promise.allSettled([
-                            fetchIssueData(apiUrl + searchParams)
-                        ])];
-                case 1:
-                    result = _a.sent();
-                    console.log(result);
-                    return [2 /*return*/];
-            }
-        });
-    });
+
+async function getUserData(username) {
+  let apiUrl = `https://gitlab.com/api/v4/users/${username}/events?`
+  let searchParamsObject = {
+    per_page: "10",
+  };
+
+  let searchParams = new URLSearchParams(searchParamsObject).toString()
+
+  const result = await Promise.allSettled([
+    fetchIssueData(apiUrl + searchParams)
+  ])
+
+  console.log(result)
 }
+
 function main() {
-    getLowerDateBoundary();
-    getUserData("mmilek");
+  getLowerDateBoundary()
+  getUserData("mmilek")
 }
-main();
+
