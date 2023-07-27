14a7b380a62c99caeaf3e51a86a1740b28151fa7
sebnow91 <sebnow91@gmail.com>
2023-07-27 22:04:43 +0200

    Add project avatar display, delete old code

--- src/display_stats.js
+++ src/display_stats.js
@@ -54,30 +54,27 @@ function createStat(prefix, stat,sufix, animation){
   
 }
 
-function createLink(description, stat,animation){
-  let div = document.createElement("div")
-  div.innerHTML = description + ": "
-  let link = document.createElement("a")
-  link.href = stat
-  link.target = "_blank"
-  link.innerHTML = stat
-  div.appendChild(link)
-  div.style.animation = animation
-  return div
+function createAvatar(avatar, link, description, animation) {
+  let mainDiv = document.createElement("div")
+  let avatarImg = document.createElement("img")
+  let anchorDiv = document.createElement("a")
+  let descriptionDiv = document.createElement("div")
+  mainDiv = randomColorPalete(mainDiv, colorPaletes)
+  mainDiv.classList.add("stats")
+  mainDiv.style.animation = animation
+  anchorDiv.href = link
+  avatarImg.src = avatar
+  avatarImg.alt = "project avatar"
+  avatarImg.style.height = "100px"
+  avatarImg.style.aspectRatio = "1 / 1"
+  anchorDiv.style.gridArea = "1 / 2 / 3 / 3"
+  descriptionDiv.innerHTML = description
+  descriptionDiv.style.gridArea = "4 / 1 / 4 / 4"
+  descriptionDiv.style.textAlign = "center"
+  anchorDiv.append(avatarImg)
+  mainDiv.append(anchorDiv, descriptionDiv, descriptionDiv)
+  return mainDiv
 }
-
-// function createImage(stat, animation){
-//   let div = document.createElement("div")
-//   div.className = "stats"
-//   let img = document.createElement("img")
-//   img.src = stat
-//   img.alt = "project avatar"
-//   img.height = 100
-//   img.width = 100
-//   div.style.animation = animation
-//   div.appendChild(img)
-//   return div
-// }
 
 // function createObjectStat(description, stat, animation){
 //   let div = document.createElement("div")
@@ -91,15 +88,15 @@ function showStats(stats){
   let statsDiv = document.getElementById("stats-div")
   statsDiv.appendChild(createStat("You have made", stats.commitData.numberOfCommits, "commits", "fadeIntoLeft 1s forwards"))
   statsDiv.appendChild(createStat("That amount to", stats.commitData.numberOfPushes, "pushes", "fadeIntoRight 2s forwards"))
-  statsDiv.appendChild(createStat("This", stats.commitData.longestCommitMessage,"is your longest commit message", "fadeInFromBelow 3s forwards"))
-  statsDiv.appendChild(createStat("On the other hand", stats.commitData.shortestCommitMessage,"this is your shortest commit message", "fadeInFromBelow 4s forwards"))
-  statsDiv.appendChild(createStat("This shows how much", stats.issueData.openedIssues, "of your issues are open" , "fadeIntoRight 5s forwards"))
-  statsDiv.appendChild(createStat("And this shows how much", stats.issueData.closedIssues, "issues you have closed" , "fadeIntoLeft 6s forwards"))
-  statsDiv.appendChild(createStat("You have pushed to", stats.commitData.numberOfProjectsPushedTo, "projects", "fadeInFromBelow 7s forwards"))
-  statsDiv.appendChild(createStat("You have commited the most", stats.commitData.projectMostPushedTo.name, "to this project", "fadeInFromAbove 8s forwards"))
-  // statsDiv.appendChild(createImage(stats.commitData.projectMostPushedTo.avatar_url, "fadeInFromBelow 2s forwards"))
-  // statsDiv.appendChild(createLink("check it out at", stats.commitData.projectMostPushedTo.web_url, "fadeInFromBelow 2s forwards"))
-  // statsDiv.appendChild(createStat("description", stats.commitData.projectMostPushedTo.description, "fadeInFromBelow 2s forwards"))
+  statsDiv.appendChild(createStat("This", stats.commitData.longestCommitMessage,"is your longest commit message", "fadeInFromBelow 2s forwards"))
+  statsDiv.appendChild(createStat("On the other hand", stats.commitData.shortestCommitMessage,"this is your shortest commit message", "fadeInFromBelow 2s forwards"))
+  statsDiv.appendChild(createStat("This shows how much", stats.issueData.openedIssues, "of your issues are open" , "fadeIntoRight 2s forwards"))
+  statsDiv.appendChild(createStat("And this shows how much", stats.issueData.closedIssues, "issues you have closed" , "fadeIntoLeft 2s forwards"))
+  statsDiv.appendChild(createStat("You have pushed to", stats.commitData.numberOfProjectsPushedTo, "projects", "fadeInFromBelow 2s forwards"))
+  statsDiv.appendChild(createStat("And now", "Your most commited project","here are the stats", "fadeInFromBelow 2s forwards"))
+  statsDiv.appendChild(createStat("You have commited the most", stats.commitData.projectMostPushedTo.name, "to this project", "fadeInFromAbove 2s forwards"))
+  statsDiv.appendChild(createAvatar(stats.commitData.projectMostPushedTo.avatar_url, stats.commitData.projectMostPushedTo.web_url,
+				    stats.commitData.projectMostPushedTo.description, "fadeInFromBelow 2s forwards"))
   // statsDiv.appendChild(createStat("number of commits", stats.commitData.projectMostPushedTo.commits, "fadeInFromBelow 2s forwards"))
   // statsDiv.appendChild(createStat("number of pushes", stats.commitData.projectMostPushedTo.pushes, "fadeInFromBelow 2s forwards"))
   // statsDiv.appendChild(createObjectStat("used languages", stats.commitData.projectMostPushedTo.languages, "fadeInFromBelow 2s forwards"))
